4925b26ccec4e1e297b898560a17b31b3d4efa54
Hirusha Adikari <hirushaadi@gmail.com>
2025-05-11 17:43:23 +0530

    add POST method

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -9,9 +9,16 @@ def index():
     return render_template("home.html")
 
 
-@app.route("/merge_pdf")
+@app.route("/merge_pdf", methods=['GET', 'POST'])
 def merge_pdf():
-    return render_template("merge_pdf.html")
+    if request.method == 'GET':
+        return render_template("merge_pdf.html")
+
+    elif request.method == 'POST':
+        # get the files
+        # store all the files in the order which im supposed to merge in a variable
+        # and also get the filename
+        ...
 
 
 if __name__ == "__main__":

d50c429c569521281289f17d125edeaed9e41fa6
Hirusha Adikari <hirushaadi@gmail.com>
2025-05-11 14:35:09 +0530

    home page setup

--- utils/initialize.py
+++ utils/initialize.py
@@ -3,7 +3,8 @@ import requests
 """
 /static/js:
     https://code.jquery.com/jquery-3.7.1.min.js
-    https://cdn.jsdelivr.net/npm/daisyui@5
     https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4
     
+/static/css:
+    https://cdn.jsdelivr.net/npm/daisyui@5
 """

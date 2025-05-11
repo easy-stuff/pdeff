44777d1f40297c16476345307563c870cab04cc9
Hirusha Adikari <hirushaadi@gmail.com>
2025-05-11 20:59:54 +0530

    split_pdf route

--- app.py
+++ app.py
@@ -36,5 +36,30 @@ def merge_pdf():
         )
 
 
+@app.route("/split_pdf", methods=['GET', 'POST'])
+def split_pdf():
+    if request.method == 'GET':
+        return render_template("split_pdf.html")
+
+    elif request.method == 'POST':
+        files = request.files.getlist('files')
+        filename = request.form.get('filename', 'merged')
+
+        final_name = utils.files.sanitize_filename(
+            filename=filename,
+            extension='.pdf'
+        )
+
+        # TODO: Change this!
+        merged_pdf = utils.pdf.merge.merge_pdfs(files)
+
+        return send_file(
+            merged_pdf,
+            as_attachment=True,
+            download_name=final_name,
+            mimetype='application/pdf'
+        )
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=8080, debug=True)
